53946c89cf1e12a96d83440a26e380f7cc5dddf7
domkirke <axelchemla@yahoo.fr>
2022-12-27 01:54:52 +0100

    added latent space

--- ddsp/model.py
+++ ddsp/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torchaudio as ta
 from .core import mlp, gru, scale_function, remove_above_nyquist, upsample
 from .core import harmonic_synth, amp_to_impulse_response, fft_convolve
 from .core import resample
@@ -37,39 +38,61 @@ class Reverb(nn.Module):
         return x
 
 
+
 class DDSP(nn.Module):
-    def __init__(self, hidden_size, n_harmonic, n_bands, sampling_rate,
-                 block_size):
+    def __init__(self, 
+                 latent_size = 16,
+                 frequency_size = 1,
+                 loudness_size = 1,
+                 hidden_size = 512,
+                 n_harmonic = 100,
+                 n_bands = 65,
+                 sampling_rate = 16000,
+                 block_size = 160, 
+                 reverb = True):
         super().__init__()
         self.register_buffer("sampling_rate", torch.tensor(sampling_rate))
         self.register_buffer("block_size", torch.tensor(block_size))
+        self.latent_size = latent_size
 
-        self.in_mlps = nn.ModuleList([mlp(1, hidden_size, 3)] * 2)
-        self.gru = gru(2, hidden_size)
-        self.out_mlp = mlp(3*hidden_size, hidden_size, 3)
+        in_mlps = [mlp(frequency_size, hidden_size, 3)]*2
+        if self.latent_size > 0:
+            in_mlps.append(mlp(latent_size, hidden_size, 3))
+        self.in_mlps = nn.ModuleList(in_mlps)
+
+        self.gru = gru(2 + int(self.latent_size > 0), hidden_size)
+        self.out_mlp = mlp((3 + int(self.latent_size > 0))*hidden_size, hidden_size, 3)
+
+        # dynamic loudness regularization
+        self.loudness_bn = nn.BatchNorm2d(loudness_size, momentum=0.0)
+        # init batchnorm to fit -80-0dB range
+        self.loudness_bn.running_mean = torch.tensor([-40.])
+        self.loudness_bn.running_var = torch.tensor([40.])
 
         self.proj_matrices = nn.ModuleList([
             nn.Linear(hidden_size, n_harmonic + 1),
             nn.Linear(hidden_size, n_bands),
         ])
 
-        self.reverb = Reverb(sampling_rate, sampling_rate)
+        self.has_reverb = reverb
+        if self.has_reverb:
+            self.reverb = Reverb(sampling_rate, sampling_rate)
 
         self.register_buffer("cache_gru", torch.zeros(1, 1, hidden_size))
         self.register_buffer("phase", torch.zeros(1))
 
-    def forward(self, pitch, loudness):
+    def forward(self, pitch, loudness, z = None):
         # hidden = torch.cat([
         #     self.in_mlps[0](pitch),
         #     self.in_mlps[1](loudness),
         # ], -1)
         # hidden = torch.cat([self.gru(hidden)[0], pitch, loudness], -1)
         # hidden = self.out_mlp(hidden)
-
-        desc_embeddings = torch.cat([
-            self.in_mlps[0](pitch),
-            self.in_mlps[1](loudness),
-        ], -1)
+        loudness = self.loudness_bn(loudness.unsqueeze(-3))[..., 0, :, :]
+        desc_inputs = [self.in_mlps[0](pitch), self.in_mlps[1](loudness)]
+        if (self.latent_size > 0) and (z is not None):
+            desc_inputs.append(self.in_mlps[2](z))
+        desc_embeddings = torch.cat(desc_inputs, -1)
         hidden = torch.cat([self.gru(desc_embeddings)[0], desc_embeddings], -1)
         hidden = self.out_mlp(hidden)
 
@@ -108,9 +131,10 @@ class DDSP(nn.Module):
         signal = harmonic + noise
 
         #reverb part
-        signal = self.reverb(signal)
+        if self.has_reverb:
+            signal = self.reverb(signal)
 
-        return signal
+        return signal[..., 0]
 
     def realtime_forward(self, pitch, loudness):
         hidden = torch.cat([
